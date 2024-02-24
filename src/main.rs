6e404d5406ef09c0c0a508ef537b7ebd827a8109
Alec Hampton <admin@thewebfromscratch.com>
2024-02-24 10:37:31 -0700

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,21 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let target_string = &args[1];
-    let file_path = &args[2];
-    println!("Searching for {target_string} in file {file_path}");
-    let contents = fs::read_to_string(file_path)
+    let config = parse_input(&args);
+    println!("Searching for {} in file {}", config.target_string, config.file_path);
+    let contents = fs::read_to_string(config.file_path)
         .expect("An error occurred when attempting to read from the specified file");
     println!("Contents read as {contents}");
+}
+
+struct Config {
+    target_string: String,
+    file_path: String,
+}
+
+fn parse_input(args: &[String]) -> Config {
+    let target_string = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { target_string, file_path }
 }
