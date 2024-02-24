beb23a433cc3c6339363972fd47b8d040b058911
Alec Hampton <admin@thewebfromscratch.com>
2024-02-23 23:53:52 -0700

    Accepting command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let target_string = &args[1];
+    let file_path = &args[2];
+    println!("Searching for {target_string} in file {file_path}");
 }
