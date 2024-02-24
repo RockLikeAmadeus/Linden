9adac4af47a8eab223e6be3b038e79b06e0ccb8d
Alec Hampton <admin@thewebfromscratch.com>
2024-02-24 10:03:32 -0700

    Reading from a file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let target_string = &args[1];
     let file_path = &args[2];
     println!("Searching for {target_string} in file {file_path}");
+    let contents = fs::read_to_string(file_path)
+        .expect("An error occurred when attempting to read from the specified file");
+    println!("Contents read as {contents}");
 }
