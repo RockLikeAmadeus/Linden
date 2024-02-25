720ac5472d7d7b7e50d5e7437a6bc2cb0267ad6c
Alec Hampton <admin@thewebfromscratch.com>
2024-02-25 15:52:15 -0700

    Added error handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("There was a problem with the arguments provided: {err}");
+        process::exit(1);
+    });
     println!("Searching for {} in file {}", config.target_string, config.file_path);
     let contents = fs::read_to_string(config.file_path)
         .expect("An error occurred when attempting to read from the specified file");
@@ -16,10 +20,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments provided. Expected string and file path.");
+        }
         let target_string = args[1].clone();
         let file_path = args[2].clone();
     
-        Config { target_string, file_path }
+        Ok(Config { target_string, file_path })
     }
 }
