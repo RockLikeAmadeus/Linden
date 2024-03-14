20516cdb3e631bdcc0dd604ef87c99e87097ee3b
Alec Hampton <admin@thewebfromscratch.com>
2024-03-14 15:48:24 -0600

    Extracted config struct and run logic into library crate

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+use std::fs;
+use std::error::Error;
+
+pub struct Config {
+    pub target_string: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments provided. Expected string and file path.");
+        }
+        let target_string = args[1].clone();
+        let file_path = args[2].clone();
+    
+        Ok(Config { target_string, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    println!("Contents read as {contents}");
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use linden::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -10,32 +10,8 @@ fn main() {
         process::exit(1);
     });
     println!("Searching for {} in file {}", config.target_string, config.file_path);
-    if let Err(e) = run(config) {
+    if let Err(e) = linden::run(config) {
         println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-        //.expect("An error occurred when attempting to read from the specified file");
-    println!("Contents read as {contents}");
-    Ok(())
-}
-
-struct Config {
-    target_string: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments provided. Expected string and file path.");
-        }
-        let target_string = args[1].clone();
-        let file_path = args[2].clone();
-    
-        Ok(Config { target_string, file_path })
     }
 }
