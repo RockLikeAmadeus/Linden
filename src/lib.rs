22767860a8939b20e1837bb1b2a5071d68cc9e47
Alec Hampton <alhampton9@gmail.com>
2024-07-18 09:48:08 -0600

    Red

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,30 @@ impl Config {
     }
 }
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
     //println!("Contents read as {contents}");
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "three";
+        let contents = "\
+1. one
+2. two
+3. three
+4. four
+5. five";
+
+        assert_eq!(vec!["3. three"], search(query, contents));
+    }
 }
